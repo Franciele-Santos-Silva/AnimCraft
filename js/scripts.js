4ed6def3c373b449669fb561ae91f6cc965a4166
Franciele-Santos-Silva <franciellysantos876@gmail.com>
2025-10-14 16:00:28 -0300

    Carrossel

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
-console.log('franciele');
+let list = document.querySelectorAll('.item')
+
+let next = document.getElementById('next')
+let prev = document.getElementById('prev')
+
+let count = list.length
+let active = 0 
+
+next.onclick = () => {
+
+    let activeOld = document.querySelector('.active')
+    activeOld.classList.remove('active')
+
+    active = active + 1 
+    list[active].classList.add('active')
+
+    }
+
+prev.onclick = () => {
+    console.log(prev);
+    
+}
+
